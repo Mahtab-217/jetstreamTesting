a2d1f12d3693948da47fb4cd8a60d5f285dca069
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-01-02 18:59:56 +0430

    refreshes code

--- database/migrations/2026_01_02_142004_create_students_table.php
+++ database/migrations/2026_01_02_142004_create_students_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("lastName");
+            $table->foreignId("user_id")->constrained("users");
             $table->timestamps();
         });
     }
