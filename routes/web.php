f269c74b6093b90804c70289ced4dc511d28bf1a
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-01-02 20:13:54 +0430

    new code

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\StudentsController;
+use App\Http\Middleware\TeacherMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,3 +17,7 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+Route::prefix('user')->controller(StudentsController::class,)->middleware(TeacherMiddleware::class)->group(function(){
+Route::get('/',[StudentsController::class, 'index']);
+});
+
