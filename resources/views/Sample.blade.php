b590ab474c2448fab94ee61bf4cdaea7197ae82b
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-01-07 20:38:27 +0430

    new-style

--- resources/views/Sample.blade.php
+++ resources/views/Sample.blade.php
@@ -5,8 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
-    
+    <div class="w-full bg-gray-300 p-12 ">
+        <nav>
+            <h1 class=" font-bold text-3xl text-center text-gray-100 "></h1>
+        </nav>
+    </div>
 </body>
 </html>
