f82404eba6b7da241cb95f60c5454e4166b96c9e
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-01-13 21:46:13 +0430

    adding style for gmail

--- resources/views/mail/verify.blade.php
+++ resources/views/mail/verify.blade.php
@@ -5,16 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+       <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
-    <div>
-        <p>
+    <div class="w-full h-screen bg-stone-300 flex justify-center items-center ">
+        <p class="text-center font-bold">
             سلام استفاده کننده عزیز خوش آمدید. ایمیل ادرس تان را وارد نمایید
         </p>
-        <a href="">
+        <a class="bg-green-600 border rounded-md py-2 px-1.5 " href="">
             تایید ایمیل آدرس
         </a>
-        <p>
+        <p class="text-center font-bold ">
             اگر این ایمیل متعلق به شمما نیست این ایمیل را نادیده بگیرید
         </p>
     </div>
